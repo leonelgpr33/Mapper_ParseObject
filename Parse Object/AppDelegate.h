5b15b5b4b5f58fae72bf3a23e8faccaf19cca92b
leonelgpr33 <leonel.perez@mrci.com.mx>
2016-01-05 18:36:38 -0600

    Initial Commit

---
+++ Parse Object/AppDelegate.h
@@ -0,0 +1,25 @@
+//
+//  AppDelegate.h
+//  Parse Object
+//
+//  Created by LI Leonel G. Pérez Ramos on 05/01/16.
+//  Copyright © 2016 LI Leonel G. Pérez Ramos. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <CoreData/CoreData.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (readonly, strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+@property (readonly, strong, nonatomic) NSManagedObjectModel *managedObjectModel;
+@property (readonly, strong, nonatomic) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+
+- (void)saveContext;
+- (NSURL *)applicationDocumentsDirectory;
+
+
+@end
+
---
+++ Parse Object/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  Parse Object
+//
+//  Created by LI Leonel G. Pérez Ramos on 05/01/16.
+//  Copyright © 2016 LI Leonel G. Pérez Ramos. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
